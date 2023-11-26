31bf37b6a3d1ee04e2f300d0f121da46b6882103
hshnj0216 <ldecena1116@gmail.com>
2023-11-27 01:29:29 +0800

    change study page buttons and input layout

--- client/src/app/(routes)/study/page.tsx
+++ client/src/app/(routes)/study/page.tsx
@@ -8,11 +8,12 @@ import Examples from "@/app/_components/(study)/Examples";
 import StrokeImages from "@/app/_components/(study)/StrokeImages";
 import Introduction from "@/app/_components/(study)/Introduction";
 import KanjiDecomposition from "@/app/_components/(study)/KanjiDecomposition";
+import Link from "next/link";
 
 
 const Study: FC = () => {
 	const {
-		kanji, 
+		kanji,
 		onKanjiSelection,
 		displayDecomposition,
 		setDisplayDecomposition,
@@ -23,29 +24,32 @@ const Study: FC = () => {
 	} = useKanji();
 	return (
 		<div className="w-4/5 mx-auto h-full">
-			<div className="flex justify-between items-center">
+			<div className="grid grid-cols-12 h-16 items-center">
+				<Link href="/" className="bg-slate-50 p-2 border rounded col-start-1 col-end-3 text-center my-3">
+					Return to Main Menu
+				</Link>
 				<SearchBar onKanjiSelection={onKanjiSelection}/>
-				{kanji ? 
-					<button 
-						type="button" 
+				{kanji &&
+					<button
+						type="button"
 						onClick={() => setDisplayDecomposition(!displayDecomposition)}
-						className="bg-slate-50 border rounded p-3 hover:bg-slate-300"
+						className="bg-slate-50 border rounded p-2 hover:bg-slate-300 col-start-10 col-end-13 my-3"
 					>
 						{displayDecomposition ? "Show information" : "Show decomposition"}
-					</button> : ""
+					</button>
 				}
 			</div>
 			{kanji ? (
 				displayDecomposition ? (
-					<KanjiDecomposition kanjiTree={kanjiDecompositionProps.kanjiTree}/>
+					<KanjiDecomposition kanjiTree={kanjiDecompositionProps.kanjiTree} />
 				) : (
 					<div className="grid lg:grid-cols-12 lg:gap-4 border-slate-50">
-						<KanjiMedia	{...kanjiMediaProps}/>
-						<KanjiInfo {...kanjiInfoProps} /> 
+						<KanjiMedia	{...kanjiMediaProps} />
+						<KanjiInfo {...kanjiInfoProps} />
 						<Examples examples={kanji?.examples}></Examples>
 						<StrokeImages {...kanjiStrokeImagesProps}></StrokeImages>
 					</div>
-				)				
+				)
 			) : (
 				<Introduction />
 			)}
--- client/src/app/_components/(study)/SearchBar.tsx
+++ client/src/app/_components/(study)/SearchBar.tsx
@@ -18,10 +18,10 @@ const SearchBar: FC<ISearchBarProps> = ({ onKanjiSelection }) => {
     } = useSearch();
 
     return (
-        <div className="my-3 grid grid-cols-12 relative w-3/12">
-            <div className="grid grid-cols-12 col-span-12">
+        <div className="col-start-4 col-end-9 my-3 grid grid-cols-12 relative">
+            <div className="grid grid-cols-12 col-span-full">
                 <input
-                    className="border border-gray-300 p-2 rounded-l-md col-span-9"
+                    className="border border-gray-300 p-2 rounded-l-md col-span-10"
                     type="text"
                     placeholder="Paste kanji character or type English word"
                     onChange={(e) => {
